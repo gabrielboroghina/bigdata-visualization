78842c649ee07f8c5223108cd85b4fe14d384b87
Mihaela Catrina <mcatrina@bitdefender.com>
2021-01-23 22:49:01 +0200

    Use mongoimport to import csv file

--- server/mongo_init.sh
+++ server/mongo_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+INPUT=/tmp/data.csv
 
 mongo <<EOF
 use '$MONGO_INITDB_DATABASE'
@@ -13,39 +14,5 @@ db.createUser({
 })
 EOF
 
+mongoimport --type csv -d $MONGO_INITDB_DATABASE -c covidinfos --headerline --drop $INPUT
 
-INPUT=/tmp/data.csv
-OLDIFS=$IFS
-IFS=','
-unixDateDelim="-"
-
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-i=1
-while read dateRep yearWeek casesWeekly deathsWeekly countriesAndTerritories geoId countryterritoryCode popData2019 continentExp notificationRatePer100000Population14Days
-do
-  test $i -eq 1 && ((i=i+1)) && continue
-  yearR=$(cut -d'/' -f3 <<< $dateRep)
-	month=$(cut -d'/' -f2 <<< $dateRep)
-	day=$(cut -d'/' -f1 <<< $dateRep)
-	date=$yearR$unixDateDelim$month$unixDateDelim$day
-	week=$(cut -d'-' -f2 <<< $yearWeek)
-  year=$(cut -d'-' -f1 <<< $yearWeek)
-  rate=$(tr -d '[:space:]' <<<$notificationRatePer100000Population14Days)
-mongo <<EOF
-use $MONGO_INITDB_DATABASE
-db.covidinfos.insert({
-  dateRep: Date('$date'),
-  year: '$year',
-  week: '$week',
-  cases_weekly: '$casesWeekly',
-  deaths_weekly: '$deathsWeekly',
-  countriesAndTerritories: '$countriesAndTerritories',
-  geoId: '$geoId',
-  countryterritoryCode: '$countryterritoryCode',
-  popData2019: '$popData2019',
-  continentExp: '$continentExp',
-  notificationRatePer100000Population14Days: '$rate'
-})
-EOF
-done < $INPUT
-IFS=$OLDIFS
