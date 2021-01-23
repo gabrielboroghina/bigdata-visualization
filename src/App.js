1f7144a85f8d57ca77314863748d795580a06c1f
Gabriel Boroghina <gabrielboroghina@outlook.com>
2021-01-23 13:19:48 +0200

    Test SVG chart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import BarChart from "./visualization/BarChart";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-            </header>
+            <h1>BigData Visualization</h1>
+            <BarChart data={[5, 10, 1, 3]} size={[500, 500]}/>
         </div>
     );
 }
